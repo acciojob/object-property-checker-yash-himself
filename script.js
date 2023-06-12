f8727a8988f9615a96fc597a8e66cf9664260cf1
Yash Gupta <yashgupta27690@gmail.com>
2023-06-12 21:35:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let k in sampleObject){
+		if (k==key) {
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 }
 
 // Do not change the code below
